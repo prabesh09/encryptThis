101873f8031f351e17c0a29d9fcae668feb53b04
Prabesh Shrestha <prabeshshrestha90@gmail.com>
2024-05-06 21:29:11 +0545

    Frontend 🐱

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div className='bg-red-500 p-5'>hello world</div>
+    <main className="p-5">
+      <h1>Text to be encrypted</h1>
+      <input type="text" className="border-black border-2 outline-none" />
+      <h1>Password</h1>
+      <input type="password" className="border-black border-2 outline-none" />
+      <br />
+      <label htmlFor="enc">Encrypt</label>
+      <input type="radio" name="enc" />
+      <label htmlFor="enc">Decrypt</label>
+      <input type="radio" name="enc" />
+      <h1>Output</h1>
+      <input type="text" disabled className="border-black border-2 outline-none" />
+    </main>
   )
 }
 
