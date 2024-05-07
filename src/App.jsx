7663cb0ba8d35fb9c756d1370ddc4bf2adac2674
Prabesh Shrestha <prabeshshrestha90@gmail.com>
2024-05-07 11:50:07 +0545

    UI change 🐱

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [input, setInput] = useState("")
   const [password, setPassword] = useState("")
   const [option, setOption] = useState("encrypt")
+  const [value, setValue] = useState("")
 
   const handleInput = (event) => {
     setInput(event.target.value);
@@ -16,39 +17,40 @@ const App = () => {
 
   const handleRadio = (event) => {
     setOption(event.target.value)
-    console.log(event.target.value)
   }
 
   const handleOutput = () => {
     if (option === "encrypt") {
       let ciphertext = CryptoJS.AES.encrypt(input, password).toString()
-      console.log(ciphertext)
+      setValue(ciphertext)
     }
 
     if (option === "decrypt") {
-      // Decrypt
-      var bytes = CryptoJS.AES.decrypt(input, password);
-      var originalText = bytes.toString(CryptoJS.enc.Utf8);
-
-      console.log(originalText);
+      let bytes = CryptoJS.AES.decrypt(input, password);
+      let originalText = bytes.toString(CryptoJS.enc.Utf8);
+      setValue(originalText);
     }
   }
 
   return (
-    <main className="p-5">
-      <h1>Text to be encrypted</h1>
-      <input type="text" className="border-black border-2 outline-none" onChange={handleInput} />
+    <main className="p-5 grid content-center h-screen">
+      <h1>Text to be encrypted/decrypted</h1>
+      <input type="text" className="border-black border-2 outline-none px-2 h-12 rounded-xl my-2" onChange={handleInput} />
       <h1>Password</h1>
-      <input type="password" className="border-black border-2 outline-none" onChange={handlePassword} />
+      <input type="password" className="border-black border-2 outline-none px-2 h-12 rounded-xl my-2" onChange={handlePassword} />
       <br />
-      <label htmlFor="enc">Encrypt</label>
-      <input type="radio" name="enc" value="encrypt" onChange={handleRadio} checked={option === "encrypt"} />
-      <label htmlFor="enc">Decrypt</label>
-      <input type="radio" name="enc" value="decrypt" onChange={handleRadio} checked={option === "decrypt"} />
-      <h1>Output</h1>
-      <input type="text" disabled placeholder="check console 😒" className="border-black border-2 outline-none" />
+      <div className="flex gap-5 items-center my-5">
+        <label htmlFor="enc">Encrypt</label>
+        <input type="radio" name="enc" value="encrypt" onChange={handleRadio} checked={option === "encrypt"} />
 
-      <button onClick={handleOutput}>find</button>
+        <label htmlFor="enc">Decrypt</label>
+        <input type="radio" name="enc" value="decrypt" onChange={handleRadio} checked={option === "decrypt"} />
+      </div>
+      <h1>Output</h1>
+
+      <input type="text" disabled placeholder="Your output will be displayed here" value={value} className="border-black border-2 outline-none px-2 h-12 rounded-xl my-2" />
+
+      <button className="bg-red-500 h-12 rounded-full font-semibold text-white my-5" onClick={handleOutput}>Output</button>
     </main>
   )
 }
